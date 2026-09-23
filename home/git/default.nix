11025894c5ecccaf525e907fd3c933d746f99ae6
DCsunset <DCsunset@protonmail.com>
2026-09-23 15:31:57 +0000

    git: update git config

--- home/git/default.nix
+++ home/git/default.nix
@@ -1,37 +1,50 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
-  programs.git = {
-    enable = true;
-    aliases = {
-      st = "status";
-      lg = "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
+  programs = {
+    git = {
+      enable = true;
+      # use canonical timezone to hide location for privacy
+      package = pkgs.osHelpers.wrapPackage pkgs.git "--set TZ Etc/UTC";
+      lfs.enable = true;
+
+      settings = {
+        alias = {
+          st = "status";
+          lg = "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
+        };
+        # recording of resolved conflicts
+        rerere.enabled = true;
+        init.defaultBranch = "main";
+        # make log --commit-graph faster for large repo
+        fetch.writeCommitGraph = true;
+        branch.sort = "-committerdate";
+        # allow using file as url
+        protocol.file.allow = "always";
+        # store credentials for HTTP
+        # credential.helper = "store --file ${appDataDir}/git-credentials";
+      };
+      ignores = [
+        # direnv
+        ".direnv"
+        # npm
+        "node_modules"
+        # python
+        "__pycache__"
+        "*.egg-info"
+        # emacs temp files
+        "*~"
+        ".#*"
+      ];
     };
-    ignores = [
-      # npm
-      "node_modules"
-      # python
-      "__pycache__"
-      "*.egg-info"
-      # emacs temp files
-      "*~"
-      ".#*"
-    ];
-    # Use https://github.com/dandavison/delta for diff
     delta = {
       enable = true;
+      enableGitIntegration = true;
       options = {
         # use n and N to move between diff sections
         navigate = true;
         line-numbers = true;
       };
-    };
-    extraConfig = {
-      init = {
-        defaultBranch = "main";
-      };
-      # allow using file as url
-      protocol.file.allow = "always";
     };
   };
 }
